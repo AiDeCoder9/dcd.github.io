4f790b10c3c85df75282fa79d36309ab5be237a8
ArchaiveArthus <35091588+ArchaiveArthus@users.noreply.github.com>
2018-10-05 08:12:02 +0545

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+
+$(function() {
+    // Smooth Scrolling
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+
+    $('.nav-activator').click(function(){
+      $('.nav-side').addClass('d-block');
+    });
+    $('#nav-dismiss').click(function(){
+      $('.nav-side').removeClass('d-block');
+    });
+  });
+
+
+  window.onscroll = function() {myFunction()};
+
+var nav = document.getElementById("navbar-example2");
+var sticky = nav.offsetTop;
+
+
+
+window.sr=ScrollReveal();
+// sr.reveal('.header',{
+//     duration:2000,
+//     origin:'top',
+//     distance:'300px',
+//     viewFactor:0.2
+// }
+// );
+
+
+
+ 
+
+  
+
